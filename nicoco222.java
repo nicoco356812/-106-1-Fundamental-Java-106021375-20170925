c4eeeb8d8849449d057e278b9ec708644da7dcba
nicoco356812 <106021375@live.asia.edu.tw>
2017-09-25 16:26:15 +0800

    20170925

---
+++ nicoco222.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class nicoco222 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x=scn.nextInt();
+        System.out.println(Math.round(x*0.00166667*10)/10f);
+    }
+}
---
+++ nicoco2222.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class nicoco2222 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        char chOld = scn.next().charAt(0);
+        char chNew = scn.next().charAt(0);
+        System.out.println(str.replace(chOld,chNew));
+    }
+}
